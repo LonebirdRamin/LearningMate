671a214abb812bbbbc823907c031704a5a541388
marklileo <phornphatmak1112@gmail.com>
2023-11-04 00:56:09 +0700

    Add addingAssignmentButton&NavBar
    - Added new icon on navigation bar (.png)
    - Finish on AddAssignmentButton
    -Finish on NavigationBar
    -Added new 4 pages (Chat, LearningZone, Planner and Profile)
    - Added npm on navigation
    
    P.S. AddAssignmentButton is tested in ChatScreen

---
+++ screens/PlannerScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const PlannerScreen = () => {
+  return (
+    <View>
+      <Text>PlannerScreen</Text>
+    </View>
+  );
+};
+
+export default PlannerScreen;
