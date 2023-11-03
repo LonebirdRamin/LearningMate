a35acbeb50d0411ec735fcb906b6d849bc870632
Sirivarakul <sirivarakuljiropakarn@gmail.com>
2023-11-03 15:57:52 +0700

    Added screens
    - Added new icon
    - Create a calendar component file
    - Create a homepage mockup screen
    - Create StyleSheet "customStyles.js" which includes the style used (View the file for more information)
    
    *Note feel free to modified the App.js file, as this current version is only used for testing*

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Homepage from './screens/Homepage';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import customStyles from './styles/customStyles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={customStyles.pageBackground}>
+      <Homepage></Homepage>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
