8deb991d680a4d73bd29af602946d257d0fcddf3
NGOMNGOM <takongbaza@gmail.com>
2023-11-05 14:21:05 +0700

    Add recording register user to firestore

--- backend/hooks/signUp.js
+++ backend/hooks/signUp.js
@@ -1,8 +1,25 @@
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { firebaseAuth } from "../database/firebaseDB";
+import { Alert } from "react-native";
 
-const signUp = async ( email, password, navigation, loadState,  setModal) => {
+const addUserToFirestore = async (uid, email) => {
+  const db = getFirestore();
+  const userData = {
+    email,
+  };
+
+  try {
+    const userRef = doc(db, "react-native-crud", uid);
+
+    await setDoc(userRef, userData);
+    console.log("User added to Firestore!");
+  } catch (error) {
+    console.log(error + "User not added to Firestore!");
+  }
+};
+
+const signUp = async (email, password, navigation, loadState, setModal) => {
   loadState(true);
   setModal(false);
   try {
@@ -16,30 +33,15 @@ const signUp = async ( email, password, navigation, loadState,  setModal) => {
       email: response.user.email,
     };
 
-    // await addUserToFirestore(response.user.uid, response.user.email);
+    await addUserToFirestore(response.user.uid, response.user.email);
 
     navigation();
   } catch (error) {
     console.log(error);
+    Alert.alert("Error", "Register failed, try again", [{ text: "Ok" }]);
   } finally {
-    loadState(false)
+    loadState(false);
   }
-
-  const addUserToFirestore = async (uid, email) => {
-    const db = getFirestore();
-    const userData = {
-      email,
-    };
-
-    try {
-      const userRef = doc(db, "react-native-crud", uid);
-
-      await setDoc(userRef, userData);
-      console.log("User added to Firestore!");
-    } catch (error) {
-      console.log(error + "User not added to Firestore!");
-    }
-  };
 };
 
 export default signUp;
