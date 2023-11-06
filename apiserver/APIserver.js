9d3c425bde10d10df908485e74cd086cc3a9abdc
greennyumbs <110111845+greennyumbs@users.noreply.github.com>
2023-11-06 18:39:28 +0700

    SignIn, SignUp, Schedule query for teacher and student, Assignment query for teacher and student
    APIserver.js => API code for query from MySQL
    queryAssignment.js => check role + query assignment for teacher and student
    signIn_scheduleAPIdone.js => check role + query schedule for teacher and student

--- apiserver/APIserver.js
+++ apiserver/APIserver.js
@@ -33,7 +33,7 @@ app.get('/api/queryTest', function (req, res, next) {       //hello => path
 app.get('/api/scheduleQuery', function (req, res, next) {
   console.log("schedule query log test");
   connection.query(
-    'SELECT cs.class_id, c.class_name, d.date_name, csd.start_time, csd.end_time FROM `student` AS `s` JOIN `class_student` AS `cs` ON cs.student_id = s.student_id JOIN `class_schedule` AS `csd` ON cs.class_id = csd.class_id JOIN `date` AS `d` ON d.date_id = csd.date_id JOIN `class` AS `c` ON c.class_id = csd.class_id ORDER BY d.date_id, csd.start_time;'
+    'SELECT cs.class_id, c.class_name, d.date_namdent` AS e, csd.start_time, csd.end_time FROM `student` AS `s` JOIN `class_stu`cs` ON cs.student_id = s.student_id JOIN `class_schedule` AS `csd` ON cs.class_id = csd.class_id JOIN `date` AS `d` ON d.date_id = csd.date_id JOIN `class` AS `c` ON c.class_id = csd.class_id ORDER BY d.date_id, csd.start_time;'
     ,
     function(err, results, fields) {
       if (err) {
@@ -48,13 +48,14 @@ app.get('/api/scheduleQuery', function (req, res, next) {
 
 app.get('/api/queryIdCard', function (req, res, next) {
   console.log("query id card");
-  //const email = req.query.email;
+  // const email = req.query.email;
   const email = "ramin.such@kmutt.ac.th"
   console.log(email);
 
   connection.query(
     'SELECT `id_card` FROM `student` WHERE academic_email = ?;',
     [email], 
+    // 'SELECT `id_card` FROM `student` WHERE academic_email = "ramin.such@kmutt.ac.th";',
     function(err, results, fields) {
       if (err) {
         console.error(err);
@@ -68,59 +69,102 @@ app.get('/api/queryIdCard', function (req, res, next) {
 });
 
 app.get('/api/checkRole', function (req, res, next) {
-  console.log("check role");
-  //const email = "ramin.such@kmutt.ac.th";
+  console.log("Begin check role");
+  // const email = "ramin.such@kmutt.ac.th";
   const email = req.query.email;
+  console.log(req.query);
   console.log(email);
 
   connection.query(
-    'SELECT * FROM (SELECT role, academic_email FROM student UNION ALL SELECT role, academic_email FROM teacher) AS combined_data WHERE academic_email = ?;',
+    'SELECT role FROM (SELECT role, academic_email FROM student UNION ALL SELECT role, academic_email FROM teacher) AS combined_data WHERE academic_email = ?;',
     [email], 
     function(err, results, fields) {
       if (err) {
         console.error(err);
         res.status(500).json({ error: 'Check role error occurred' });
       } else {
+        console.log(results);
         res.json(results);
       }
     }
   );
-  console.log("check role");
+  console.log("Done check role");
 });
 
-// function getStudentSchedule(email, res) {
-//   connection.query(
-//     'SELECT s.student_name, cs.class_id, c.class_name, d.date_name, csd.start_time, csd.end_time FROM `student` AS `s` JOIN `class_student` AS `cs` ON cs.student_id = s.student_id JOIN `class_schedule` AS `csd` ON cs.class_id = csd.class_id JOIN `date` AS `d` ON d.date_id = csd.date_id JOIN `class` AS `c` ON c.class_id = csd.class_id WHERE s.academic_email = ? ORDER BY d.date_id, csd.start_time;',
-//     [email],
-//     function(err, studentResults, fields) {
-//       if (err) {
-//         console.error(err);
-//         res.status(500).json({ error: 'Student schedule query error occurred' });
-//       } else {
-//         console.log("success student query");
-//         res.json(studentResults);
-//         console.log("Student In");
-//       }
-//     }
-//   );
-// }
+app.get('/api/getStudentSchedule', function (req, res, next) {
+  console.log("Query Student Schedule");
+  const email = req.query.email;
+  connection.query(
+    'SELECT cs.class_id, c.class_name, d.date_name, csd.start_time, csd.end_time FROM `student` AS `s` JOIN `class_student` AS `cs` ON cs.student_id = s.student_id JOIN `class_schedule` AS `csd` ON cs.class_id = csd.class_id JOIN `date` AS `d` ON d.date_id = csd.date_id JOIN `class` AS `c` ON c.class_id = csd.class_id WHERE s.academic_email = ? ORDER BY d.date_id, csd.start_time;',
+    [email],
+    function(err, studentResults, fields) {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Student schedule query error occurred' });
+      } else {
+        console.log("success student query");
+        res.json(studentResults);
+        console.log("Student In");
+      }
+    }
+  );
+});
 
-// function getTeacherSchedule(email, res) {
-//   connection.query(
-//     'SELECT s.teacher_name, cs.class_id, c.class_name, d.date_name, csd.start_time, csd.end_time FROM `teacher` AS `s` JOIN `class_lecturer` AS `cs` ON cs.teacher_id = s.teacher_id JOIN `class_schedule` AS `csd` ON cs.class_id = csd.class_id JOIN `date` AS `d` ON d.date_id = csd.date_id JOIN `class` AS `c` ON c.class_id = csd.class_id WHERE s.academic_email = ? ORDER BY d.date_id, csd.start_time;',
-//     [email],
-//     function(err, teacherResults, fields) {
-//       if (err) {
-//         console.error(err);
-//         res.status(500).json({ error: 'Teacher schedule query error occurred' });
-//       } else {
-//         res.json(teacherResults);
-//         console.log("teacher IN");
-//       }
-//     }
-//   );
-// }
+app.get('/api/getTeacherSchedule', function (req, res, next) {
+  console.log("Query Teacher Schedule");
+  const email = req.query.email;
+  connection.query(
+    'SELECT cs.class_id, c.class_name, d.date_name, csd.start_time, csd.end_time FROM `teacher` AS `s` JOIN `class_lecturer` AS `cs` ON cs.teacher_id = s.teacher_id JOIN `class_schedule` AS `csd` ON cs.class_id = csd.class_id JOIN `date` AS `d` ON d.date_id = csd.date_id JOIN `class` AS `c` ON c.class_id = csd.class_id WHERE s.academic_email = ? ORDER BY d.date_id, csd.start_time;',
+    [email],
+    function(err, teacherResults, fields) {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Teacher schedule query error occurred' });
+      } else {
+        res.json(teacherResults);
+        console.log("teacher IN");
+      }
+    }
+  );
+});
 
-app.listen(5000, function () {
-  console.log('CORS-enabled web server listening on port 5000')
+app.get('/api/getTeacherAssignment', function (req, res, next) {
+  console.log("Query Teacher Assignment");
+  const email = req.query.email;
+  connection.query(
+    'SELECT a.class_id, a.assignment_name, a.assignment_publish_date, a.assignment_due_date FROM assignment AS a JOIN class_lecturer AS cl ON cl.class_id = a.class_id JOIN teacher AS t ON t.teacher_id = cl.teacher_id WHERE t.academic_email = ?;',
+    [email],
+    function(err, teacherResults, fields) {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Teacher assignment query error occurred' });
+      } else {
+        res.json(teacherResults);
+        console.log("teacher IN");
+      }
+    }
+  );
+});
+
+app.get('/api/getStudentAssignment', function (req, res, next) {
+  console.log("Query Student Assignment");
+  const email = req.query.email;
+  connection.query(
+    'SELECT a.class_id, a.assignment_name, a.assignment_publish_date, a.assignment_due_date FROM assignment AS a JOIN class_student AS cl ON cl.class_id = a.class_id JOIN student AS t ON t.student_id = cl.student_id WHERE t.academic_email = ?;',
+    [email],
+    function(err, teacherResults, fields) {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Teacher assignment query error occurred' });
+      } else {
+        res.json(teacherResults);
+        console.log("teacher IN");
+      }
+    }
+  );
+});
+
+
+app.listen(5001, function () {
+  console.log('CORS-enabled web server listening on port 5001')
 })
