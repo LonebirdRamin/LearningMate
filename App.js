e0503899568b0c734304bab87af677f0b93d6cb0
Sirivarakul <sirivarakuljiropakarn@gmail.com>
2023-12-04 15:31:35 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,7 @@
-import { StyleSheet, Text, View } from "react-native";
 import LandingStack from "./routes/LandingStack";
-import ConfirmModal from "./components/Register/ConfirmModal";
-import { SafeAreaView } from "react-native-safe-area-context";
-import customStyles from "./styles/customStyles";
-import Homepage from "./screens/Homepage";
+
 
 import { LogBox } from "react-native";
-import TempRamin from "./components/tempRamin";
-import ChatScreen from "./screens/ChatScreen";
 LogBox.ignoreLogs(["Warning: ..."]); // Ignore log notification by message
 LogBox.ignoreAllLogs(); //Ignore all log notifications
 
